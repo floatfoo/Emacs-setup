3d8e41e7de830416c4cd9e036901aba82dc88374
floatfoo <floatfoo@gmail.com>
2022-10-28 21:36:45 +0300

    line numbers and customization

--- init.el
+++ init.el
@@ -5,15 +5,20 @@
 (set-frame-font "Hack Nerd Font 13" nil t)
 
 ;; display line numbers
-(setq display-line-numbers `relative)
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+;; disable lines for some modes
+(dolist (mode '(org-mode-hook
+		vterm-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Just cleaning up an interface
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-
-;; theme
-(load-theme 'deeper-blue)
 
 ;; smooth scrolling
 (setq redisplay-dont-pause t
@@ -51,6 +56,9 @@
 (straight-use-package 'ivy)
 (straight-use-package 'counsel)
 (straight-use-package 'swiper)
+(straight-use-package 'doom-modeline)
+(straight-use-package 'doom-themes)
+(straight-use-package 'vterm)
 
 
 ;; enable vim tildas
@@ -59,12 +67,22 @@
 ;; smartparens global-mode
 (smartparens-global-mode t)
 
+;; doom-modeline init
+(doom-modeline-mode 1)
+
+;; theme
+(load-theme 'doom-acario-dark)
+
 ;; ENTER key parents autoinden 
 (defun indent-between-pair (&rest _ignored)
   (newline)
   (indent-according-to-mode)
   (forward-line -1)
   (indent-according-to-mode))
+
+;; doom-modeline setup
+(setq doom-modeline-height 25)
+(setq doom-modeline-bar-width 4)
 
 (sp-local-pair 'prog-mode "{" nil :post-handlers '((indent-between-pair "RET")))
 (sp-local-pair 'prog-mode "[" nil :post-handlers '((indent-between-pair "RET")))
@@ -73,6 +91,8 @@
 (require 'evil)
 (evil-mode 1)
 (ivy-mode 1)
+(counsel-mode 1)
+(projectile-mode +1)
 
 ;; key settings
 (global-set-key
@@ -88,4 +108,18 @@
 (global-set-key
  (kbd "<escape>")
  'keyboard-escape-quit
+ )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
